b7af7d0dbc929e3f0c816aaa59651915f04fec36
Keyoru <Kyeur@proton.me>
2023-07-21 16:40:20 +0300

    merged from staging

--- src/main/java/scheduler/FileReader.java
+++ src/main/java/scheduler/FileReader.java
@@ -1,4 +1,5 @@
 package scheduler;
+
 import java.io.*;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -6,6 +7,10 @@ import java.time.LocalTime;
 import java.util.UUID;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -14,6 +19,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class FileReader {
     private File file;
+    Queue<UUID> CoursesQueue = new LinkedList<UUID>();
 
     public FileReader(File f) {
         this.file = f;
@@ -21,13 +27,20 @@ public class FileReader {
 
     public HashMap<UUID, course> readCoursesFromSheet() throws IOException {
        HashMap<UUID, course> Courses = new HashMap<UUID, course>();
+       
 
         FileInputStream fis = new FileInputStream(file);
         Workbook workbook = new XSSFWorkbook(fis);
 
         // Get the specific sheet by name
-        Sheet sheet1 = workbook.getSheetAt(1);
-        Sheet sheet2 = workbook.getSheetAt(0);
+        Sheet sheet1 = workbook.getSheetAt(0);
+        Sheet sheet2 = workbook.getSheetAt(1);
+
+       if(!isFirstSheet(sheet1)){
+        Sheet temp_Sheet = sheet1;
+        sheet1 = sheet2;
+        sheet2 = temp_Sheet;
+       }
 
         if (sheet1 != null && sheet2 != null) {
             // Iterate over rows in both sheets simultaneously
@@ -54,7 +67,7 @@ public class FileReader {
                   int index2 = slots[1];
 
 
-                    LinkedList<String>Split_Days = Split_String(instructor_days,"/");
+                    LinkedList<String>Split_Days = Split_Days(instructor_days);
                     LinkedList<Integer>Instructor_days = new LinkedList<Integer>();
 
                     for(int i=0;i<Split_Days.size();i++){
@@ -62,7 +75,7 @@ public class FileReader {
                    }
 
                     String conflict_courses  = row2.getCell(0).getStringCellValue();
-                    LinkedList<String> conflicting_courses = Split_String(conflict_courses,"/");
+                    LinkedList<String> conflicting_courses = Split_Days(conflict_courses);
 
                     String course_type = row2.getCell(1).getStringCellValue();
                     String session_time = row2.getCell(2).getStringCellValue();
@@ -71,10 +84,11 @@ public class FileReader {
               
 
                       course course = new  course(course_id, course_name, num_credits,num_sections,num_sessions, instructor_name,
-                      Instructor_days, index1, index2, conflicting_courses , course_type , calculateSlots(session_time));
+                      Instructor_days, index1, index2, conflicting_courses , course_type , calculateSlots(session_time),session_time);
 
-        
-                      Courses.put(UUID.randomUUID() , course);
+                    UUID courseUUID = UUID.randomUUID();   
+                    Courses.put(courseUUID , course);
+                    CoursesQueue.add(courseUUID);
                 }
             }
          }
@@ -82,10 +96,7 @@ public class FileReader {
          fis.close();
  
          return Courses;
-    }
-        
-
-
+         }
 
     public static int calculateSlots(String duration) {
         String[] parts = duration.split("\\s+");
@@ -94,42 +105,44 @@ public class FileReader {
         int minutes = 0;
 
         for (int i = 0; i < parts.length; i++) {
-            if ( parts[i].equalsIgnoreCase("hour")  || parts[i].equalsIgnoreCase("hours") ) {
+            if (parts[i].equalsIgnoreCase("hour") || parts[i].equalsIgnoreCase("hours")) {
                 hours = Integer.parseInt(parts[i - 1]);
-            } else if (parts[i].equalsIgnoreCase("min") || parts[i].equalsIgnoreCase("mins") || parts[i].equalsIgnoreCase("minutes")) {
+            } else if (parts[i].equalsIgnoreCase("min") || parts[i].equalsIgnoreCase("mins")
+                    || parts[i].equalsIgnoreCase("minutes")) {
                 minutes = Integer.parseInt(parts[i - 1]);
-            }
-            else if(parts[i].equalsIgnoreCase("internship") || parts[i].equalsIgnoreCase("no")){
+            } else if (parts[i].equalsIgnoreCase("internship") || parts[i].equalsIgnoreCase("no")) {
                 return 0;
             }
         }
 
         int totalMinutes = (hours * 60) + minutes;
-        int slots = (int) Math.ceil((double) totalMinutes / 75);
+        int slots = totalMinutes / 75;
 
+        if (slots == 0) {
+            slots = 1;
+        }
 
         return slots;
     }
 
     private boolean isFirstSheet(Sheet sheet) {
-     // Get the first row in the sheet
-      Row firstRow = sheet.getRow(0);
-       if (firstRow != null) {
-        // Iterate over cells in the first row
-           for (Cell cell : firstRow) {
-             // Check the header value of a specific column
-            if (cell.getColumnIndex() == 0 && cell.getStringCellValue().equalsIgnoreCase("course code")) {
-             return true;
-           }
-           else{
-            break;
-           }
-         }
+        // Get the first row in the sheet
+        Row firstRow = sheet.getRow(0);
+        if (firstRow != null) {
+            // Iterate over cells in the first row
+            for (Cell cell : firstRow) {
+                // Check the header value of a specific column
+                if (cell.getStringCellValue().equalsIgnoreCase("course id")) {
+                    return true;
+                } else {
+                    break;
+                }
+            }
+        }
+        return false;
     }
-   return false;
-  }
 
-     private int getDayIndex(String day) {
+    private int getDayIndex(String day) {
         switch (day) {
             case "Monday":
                 return 0;
@@ -146,7 +159,7 @@ public class FileReader {
         }
     }
 
-     private LinkedList<String> convertHourstoSlots(String instructorHours){
+    private LinkedList<String> convertHourstoSlots(String instructorHours) {
         LinkedList<String> slots = new LinkedList<>();
         String[] hours = instructorHours.split("/");
 
@@ -157,9 +170,9 @@ public class FileReader {
         return slots;
     }
 
-    private LinkedList<String> Split_String(String LongString, String Splitter){ //splits long string according to 
+    private LinkedList<String> Split_Days(String instructors_day) {
         LinkedList<String> slots = new LinkedList<String>();
-        String[] hours = LongString.split(Splitter);
+        String[] hours = instructors_day.split("/");
 
         for (String hour : hours) {
             slots.add(hour.trim());
@@ -168,30 +181,30 @@ public class FileReader {
         return slots;
     }
 
-
-      private int[] getSlotsIndicies(String hour1, String hour2) {
+    private int[] getSlotsIndicies(String hour1, String hour2) {
         LocalTime startTime = LocalTime.parse(hour1);
         LocalTime endTime = LocalTime.parse(hour2);
 
-
         LocalTime[] slots = {
-            LocalTime.parse("08:00"), // Slot 0
-            LocalTime.parse("09:30"), // Slot 1
-            LocalTime.parse("11:00"), // Slot 2
-            LocalTime.parse("13:00"), // Slot 3
-            LocalTime.parse("14:30"), // Slot 4
-            LocalTime.parse("16:00"), // Slot 5
-            LocalTime.parse("17:15")  // Final hour
+                LocalTime.parse("08:00"), // Slot 0
+                LocalTime.parse("09:30"), // Slot 1
+                LocalTime.parse("11:00"), // Slot 2
+                LocalTime.parse("13:00"), // Slot 3
+                LocalTime.parse("14:30"), // Slot 4
+                LocalTime.parse("16:00"), // Slot 5
+                LocalTime.parse("17:15") // Final hour
         };
 
         int startSlot = -1;
         int endSlot = -1;
 
         // Find the first slot
-        if((startTime.isAfter(LocalTime.parse("12:15")) && startTime.isBefore(LocalTime.parse("13:00"))) // between 12:15 and 1 exclusive
-            || startTime.equals(LocalTime.parse("12:15"))){
-                startSlot = 3;
-        }else{
+        if ((startTime.isAfter(LocalTime.parse("12:15")) && startTime.isBefore(LocalTime.parse("13:00"))) // between
+                                                                                                          // 12:15 and 1
+                                                                                                          // exclusive
+                || startTime.equals(LocalTime.parse("12:15"))) {
+            startSlot = 3;
+        } else {
             boolean slotFound = false;
             for (int i = 0; i < slots.length; i++) {
                 if (startTime.isBefore(slots[i]) || startTime.equals(slots[i])) {
@@ -200,28 +213,29 @@ public class FileReader {
                     break;
                 }
             }
-            if(!slotFound){
-                startSlot = slots.length-2;
+            if (!slotFound) {
+                startSlot = slots.length - 2;
             }
         }
-// Find the last slot
-        if((endTime.isAfter(LocalTime.parse("12:15")) && endTime.isBefore(LocalTime.parse("13:00"))) // between 12:15 and 1 exclusive
-            || endTime.equals(LocalTime.parse("12:15"))){
+        // Find the last slot
+        if ((endTime.isAfter(LocalTime.parse("12:15")) && endTime.isBefore(LocalTime.parse("13:00"))) // between 12:15
+                                                                                                      // and 1 exclusive
+                || endTime.equals(LocalTime.parse("12:15"))) {
             endSlot = 3;
-        }else{
+        } else {
             boolean slotFound = false;
             for (int i = startSlot; i < slots.length; i++) {
                 if (endTime.isBefore(slots[i]) || endTime.equals(slots[i])) {
-                    endSlot = i-1;
+                    endSlot = i - 1;
                     slotFound = true;
                     break;
                 }
-                if(!slotFound){
-                    endSlot = slots.length-1;
+                if (!slotFound) {
+                    endSlot = slots.length - 1;
                 }
             }
         }
         // Return the viable slots as an array
-        return new int[]{startSlot, endSlot};
+        return new int[] { startSlot, endSlot };
     }
 }
